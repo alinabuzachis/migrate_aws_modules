48eced0b1c8bd0c666ee5145b15d6a6fc26cf68e
Alina Buzachis <abuzachis@redhat.com>
2021-07-20 15:13:32 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# migrate_aws_modules
+# AWS modules migration script from community.aws to amazon.aws
+
+
+## Usage
+Create a GitHub personal access token to use in place of a password with the API and set it withing the username inside run.sh. In addition, please specify the name of the AWS module you want to migrate.
+```bash
+$ export GITHUB_TOKEN="Token ..."
+$ export USERNAME="github username"
+$ module_to_migrate=ec2_vpc_igw
+```
+Plase pecify the path to this folder.
+```bash
+$ main_folder_scripts=/path_to_the_folder
+```
+Finally, run the script.
+```bash
+$ ./run.sh
+```
--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@ module_to_migrate=ec2_vpc_igw
 c_a_path=/Users/alinabuzachis/dev/repo_migration/community.aws
 a_a_path=/Users/alinabuzachis/dev/repo_migration/amazon.aws
 
+main_folder_scripts=/Users/alinabuzachis/dev/migrate_aws_modules
+
 export GITHUB_TOKEN="Token "
 export USERNAME="username"
 
@@ -17,7 +19,7 @@ git checkout -B promote_$module_to_migrate origin/main
 git log --pretty=tformat:%H --topo-order > /tmp/change_sha1.txt
 
 # add an URL pointing on the original commit in the commit message
-FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f --msg-filter 'python3 /Users/alinabuzachis/dev/migrate_aws_modules/rewrite.py'
+FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f --msg-filter "python3 $main_folder_scripts/rewrite.py"
 
 # remove all the files, except the modules we want to keep
 FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f --prune-empty --index-filter 'git ls-tree -r --name-only --full-tree $GIT_COMMIT | \
@@ -50,7 +52,7 @@ sed -i '' '/'$module_to_migrate'/d' tests/sanity/*.txt
 git add tests/sanity/*.txt
 git commit -m "Update ignore files" 
 
-python3 /Users/alinabuzachis/dev/migrate_aws_modules/regenerare_runtime.py ${c_a_path} ${a_a_path} $module_to_migrate
+python3 $main_folder_scipts/regenerare_runtime.py ${c_a_path} ${a_a_path} $module_to_migrate
 
 cd ${a_a_path}
 git add meta/runtime*
@@ -73,4 +75,4 @@ git commit -m "Add changelog fragment"
 git push origin promote_$module_to_migrate --force
 
 sleep 10
-python /Users/alinabuzachis/dev/migrate_aws_modules/open_pr.py $module_to_migrate promote_$module_to_migrate
+python $main_folder_scipts/open_pr.py $module_to_migrate promote_$module_to_migrate
